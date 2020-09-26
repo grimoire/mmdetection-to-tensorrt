1096e22fdf61ab577497fc079fbba502276e4840
grimoire <streetyao@live.com>
2020-09-26 15:13:32 +0800

    bug fixing

--- mmdet2trt/converters/mmdet2trtOps.py
+++ mmdet2trt/converters/mmdet2trtOps.py
@@ -6,19 +6,19 @@ from torch2trt.plugins import create_meshgrid_plugin
 @tensorrt_converter('mmdet2trt.ops.adaptive_max_pool2d_by_input')
 def convert_adaptive_max_pool2d_by_input(ctx):
     input = get_arg(ctx, 'x', pos=0, default=None)
-    shape_warper = get_arg(ctx, 'shape_warper', pos=1, default=None)
+    shape_wraper = get_arg(ctx, 'shape_wraper', pos=1, default=None)
     output = ctx.method_return
 
-    output_size = shape_warper.shape
+    output_size = shape_wraper.shape
     input_trt = trt_(ctx.network, input)
-    warpshape_trt = trt_(ctx.network, shape_warper)
+    wrapshape_trt = trt_(ctx.network, shape_wraper)
 
     plugin = create_adaptivepool_plugin("adaptive_max_pool2d_by_input_"+str(id(input)),
                                         output_size=output_size,
                                         pooling_type=trt.PoolingType.MAX)
 
     layer = ctx.network.add_plugin_v2(
-        inputs=[input_trt, warpshape_trt], plugin=plugin)
+        inputs=[input_trt, wrapshape_trt], plugin=plugin)
 
     output._trt = layer.get_output(0)
 
