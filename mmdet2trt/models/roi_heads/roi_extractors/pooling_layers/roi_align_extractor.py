edfe5dbdeb1aba078d65e1201e4c235bc8cc4ec2
grimoire <streetyao@live.com>
2020-11-30 14:43:25 +0000

    fix roi_align bugs

--- mmdet2trt/models/roi_heads/roi_extractors/pooling_layers/roi_align_extractor.py
+++ mmdet2trt/models/roi_heads/roi_extractors/pooling_layers/roi_align_extractor.py
@@ -12,4 +12,28 @@ class RoiAlignExtractor(nn.Module):
         self.finest_scale = self.module.finest_scale
 
     def forward(self, feats, rois, roi_scale_factor=None):
-        return self.module(feats, rois, roi_scale_factor)
+        """Forward function."""
+        module = self.module
+        out_size = module.roi_layers[0].output_size
+        num_levels = len(feats)
+        roi_feats = feats[0].new_zeros(
+            rois.size(0), module.out_channels, *out_size)
+
+        target_lvls = module.map_roi_levels(rois, num_levels)
+        if roi_scale_factor is not None:
+            rois = module.roi_rescale(rois, roi_scale_factor)
+
+        for i in range(num_levels):
+            mask = target_lvls == i
+            inds = mask
+            if inds.any():
+                rois_ = rois[inds, :]
+                roi_feats_t = module.roi_layers[i](feats[i], rois_)
+                roi_feats[inds] = roi_feats_t
+            else:
+                roi_feats += sum(
+                    x.view(-1)[0]
+                    for x in module.parameters()) * 0. + feats[i].sum() * 0.
+        return roi_feats
+
+        # return self.module(feats, rois, roi_scale_factor)
