7d370a76bd77387bda6b7beaa5883030e498d405
Natthapawn Hongcharoen <palm22180@gmail.com>
2021-05-26 14:26:47 +0700

    Remove unnecessary imports

--- mmdet2trt/__init__.py
+++ mmdet2trt/__init__.py
@@ -1,8 +1,4 @@
-from .apis import *  # noqa: F401,F403
 from .converters import *  # noqa: F401,F403
 from .mmdet2trt import Int8CalibDataset, mask_processor2trt, mmdet2trt
-from .models import *  # noqa: F401,F403
-from .ops import *  # noqa: F401,F403
-from .core import *  # noqa: F401,F403
 
 __all__ = ['Int8CalibDataset', 'mask_processor2trt', 'mmdet2trt']
